3a2f6b0818a13a6ad955acc085a9da4e6c74e3a0
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-07-29 16:26:55 -0400

    completed game with readme

---
+++ README.md
@@ -0,0 +1,25 @@
+**WELCOME TO THE UFO GAME**
+
+**Description**
+
+Oh no! Alien invaders have arrived and are abducting humans using tractor beams. It is up you to crack the codeword and earn your medal of honor by putting a stop to the abduction!
+
+**System Requirements**
+
+Things you need in order to play this game:
+
+  * ruby 2.6.1
+  * A command line interface / terminal
+
+** Start the game **
+
+In order to play this game navigate to the Codecademy folder and then in the command line type in
+
+```ruby ufo.rb```
+
+This command should get the game up and running. If at any point you wish to exit the program, you can always quit by pressing the control key and the C key at the same time. To restart the game after exiting type in the command above.
+
+Have fun!
+
+**Questions**
+If you have any questions on running this program please contact <susmita.bhowmik516@gmail.com>
--- ufo.rb
+++ ufo.rb
@@ -1,167 +1,197 @@
 
-@x = ["                 .
-                 |
-              .-\"^\"-.
-             /_....._\\
-         .-\"`         `\"-.
-        (  ooo  ooo  ooo  )
-         '-.,_________,.-'
-              /     \\
-             /       \\
-            /    O    \\
-           /   --|--   \\
-          /      |      \\
-         /      / \\      \\
-",
-"                 .
-                 |
-              .-\"^\"-.
-             /_....._\\
-         .-\"`         `\"-.
-        (  ooo  ooo  ooo  )
-         '-.,_________,.-'
-              /     \\
-             /   O   \\
-            /  --|--  \\
-           /     |     \\
-          /     / \\     \\
-         /               \\
-",
-"                 .
-                 |
-              .-\"^\"-.
-             /_....._\\
-         .-\"`         `\"-.
-        (  ooo  ooo  ooo  )
-         '-.,_________,.-'
-              /  O  \\
-             / --|-- \\
-            /    |    \\
-           /    / \\    \\
-          /             \\
-         /               \\
-",
-"                 .
-                 |
-              .-\"^\"-.
-             /_....._\\
-         .-\"`         `\"-.
-        (  ooo  ooo  ooo  )
-         '-.,_________,.-'
-              /--|--\\
-             /   |   \\
-            /   / \\   \\
-           /           \\
-          /             \\
-         /               \\
-",
-"                 .
-                 |
-              .-\"^\"-.
-             /_....._\\
-         .-\"`         `\"-.
-        (  ooo  ooo  ooo  )
-         '-.,_________,.-'
-              /  |  \\
-             /  / \\  \\
-            /         \\
-           /           \\
-          /             \\
-         /               \\
-",
-"
-                 .
-                 |
-              .-\"^\"-.
-             /_....._\\
-         .-\"`         `\"-.
-        (  ooo  ooo  ooo  )
-         '-.,_________,.-'
-              / / \\ \\
-             /       \\
-            /         \\
-           /           \\
-          /             \\
-         /               \\
-",
-"
-                 .
-                 |
-              .-\"^\"-.
-             /_....._\\
-         .-\"`         `\"-.
-        (  ooo  ooo  ooo  )
-         '-.,_________,.-'
-              /     \\
-             /       \\
-            /         \\
-           /           \\
-          /             \\
-         /               \\
-"]
+def ufo_game
+  @x = ["                 .
+                   |
+                .-\"^\"-.
+               /_....._\\
+           .-\"`         `\"-.
+          (  ooo  ooo  ooo  )
+           '-.,_________,.-'
+                /     \\
+               /       \\
+              /    O    \\
+             /   --|--   \\
+            /      |      \\
+           /      / \\      \\
+  ",
+  "                 .
+                   |
+                .-\"^\"-.
+               /_....._\\
+           .-\"`         `\"-.
+          (  ooo  ooo  ooo  )
+           '-.,_________,.-'
+                /     \\
+               /   O   \\
+              /  --|--  \\
+             /     |     \\
+            /     / \\     \\
+           /               \\
+  ",
+  "                 .
+                   |
+                .-\"^\"-.
+               /_....._\\
+           .-\"`         `\"-.
+          (  ooo  ooo  ooo  )
+           '-.,_________,.-'
+                /  O  \\
+               / --|-- \\
+              /    |    \\
+             /    / \\    \\
+            /             \\
+           /               \\
+  ",
+  "                 .
+                   |
+                .-\"^\"-.
+               /_....._\\
+           .-\"`         `\"-.
+          (  ooo  ooo  ooo  )
+           '-.,_________,.-'
+                /--|--\\
+               /   |   \\
+              /   / \\   \\
+             /           \\
+            /             \\
+           /               \\
+  ",
+  "                 .
+                   |
+                .-\"^\"-.
+               /_....._\\
+           .-\"`         `\"-.
+          (  ooo  ooo  ooo  )
+           '-.,_________,.-'
+                /  |  \\
+               /  / \\  \\
+              /         \\
+             /           \\
+            /             \\
+           /               \\
+  ",
+  "
+                   .
+                   |
+                .-\"^\"-.
+               /_....._\\
+           .-\"`         `\"-.
+          (  ooo  ooo  ooo  )
+           '-.,_________,.-'
+                / / \\ \\
+               /       \\
+              /         \\
+             /           \\
+            /             \\
+           /               \\
+  ",
+  "
+                   .
+                   |
+                .-\"^\"-.
+               /_....._\\
+           .-\"`         `\"-.
+          (  ooo  ooo  ooo  )
+           '-.,_________,.-'
+                /     \\
+               /       \\
+              /         \\
+             /           \\
+            /             \\
+           /               \\
+  "]
+  
+  @dictionary = File.readlines("dictionary.txt").sample.delete_suffix("\n")
+  puts @dictionary
+  
+  puts "UFO: The Game"
+  puts "Instructions: save us from alien abduction by guessing letters in the codeword."
+  puts @x[0]
+  puts "Incorrect Guesses: None"
+  
+  @codeword = ""
+  @dictionary.length.times do
+    @codeword += "_"
+  end
+  
+  @codeword_with_spaces = @codeword.scan(/.{1}/).join(' ')
+  
+  p @dictionary
+  # p @codeword_with_spaces
+    
+  
+  
+  @incorrect = []
+  @correct = []
+  
+  @iterator = 0
+  
+  
+  # p @codeword
+  # p @codeword_with_spaces
 
-@dictionary = File.readlines("dictionary.txt").sample.delete_suffix("\n")
-puts @dictionary
 
-puts "UFO: The Game"
-puts "Instructions: save us from alien abduction by guessing letters in the codeword."
-puts @x[0]
-puts "Incorrect Guesses: None"
+  while (@codeword.downcase() != @dictionary && @iterator < 6)
+    puts "Codeword: #{@codeword_with_spaces}"
+    puts "Please enter your guess:"
 
-@codeword = ""
-@dictionary.length.times do
-  @codeword += "_"
+    guess = gets.chomp.downcase()
+
+    if (guess =~ /\b[[:alpha:]]\b/) == nil
+      puts "I cannot understand your input. Please guess a single letter." 
+    elsif @codeword_with_spaces.include?(guess.upcase()) == true
+      puts "You can only guess that letter once, please try again."
+    elsif @dictionary.include?(guess) == true
+      indexes = []
+      i = 0
+      while @dictionary.length > i
+        if @dictionary[i] == guess
+          indexes << i
+        end
+        i += 1
+      end
+      # p indexes
+      indexes.each do |index|
+        # p index
+        # p guess
+        @codeword[index] = guess.upcase()
+        @codeword_with_spaces = @codeword.scan(/.{1}/).join(' ')
+        puts @codeword_with_spaces
+        # p @codeword.downcase()
+        # p @dictionary
+      end 
+      if @codeword.downcase() == @dictionary
+        puts "Correct! You saved the person and earned a medal of honor!"
+        puts "The codeword is: #{@dictionary.upcase()}"
+      else
+        puts "Correct! You're closer to cracking the codeword."
+        puts @x[@iterator]
+      end
+    else  
+      @incorrect << guess.upcase()
+      @iterator += 1
+      if @iterator != 6
+        puts "Incorrect! The tractor beam pulls the person in further."
+       else
+         puts "Sorry you have been captured!"
+         puts "The codeword was #{@dictionary.upcase()}"
+      end
+      puts @x[@iterator]
+      puts "Incorrect guesses: #{@incorrect*" "}"
+    # puts @incorrect
+    end
+  # p @iteratorend
+  end
 end
 
-p @dictionary
-# p @codeword_with_spaces
-  
+ufo_game()
 
-
-@incorrect = []
-
-@iterator = 0
-
-
-p @codeword
-p @codeword_with_spaces
-
-while (@codeword.downcase() != @dictionary && @iterator <= 6)
-  puts "Codeword: #{@codeword_with_spaces}"
-  puts "Please enter your guess:"
-
-  guess = gets.chomp.downcase()
-  if @dictionary.include?(guess) == true
-    indexes = []
-    i = 0
-    while @dictionary.length > i
-      if @dictionary[i] == guess
-        indexes << i
-      end
-      i += 1
-    end
-    p indexes
-    indexes.each do |index|
-      # p index
-      # p guess
-      @codeword[index] = guess.upcase()
-      @codeword_with_spaces = @codeword.scan(/.{1}/).join(' ')
-      p @codeword_with_spaces
-      p @codeword.downcase()
-      p @dictionary
-    end 
-    puts "Correct! You're closer to cracking the codeword."
-    puts @x[@iterator]
-  else  
-    @incorrect << guess.upcase()
-    @iterator += 1
-    if @iterator != 6
-      puts "Incorrect! The tractor beam pulls the person in further."
-    else
-      puts "Sorry you have been captured!"
-    end
-    # puts @x[@iterator]
-    # puts @incorrect
-  end
-  p @iterator
+puts "Would you like to play again (Y/N)?"
+response = gets.chomp
+if response.upcase == "Y"
+  ufo_game
+elsif response.upcase == "N"
+  puts "Goodbye!"
+else
+  puts "Sorry, I cannot understand your input. Please enter Y or N"
 end
